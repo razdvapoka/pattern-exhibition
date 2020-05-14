a0395875fda277be94bafab323e722d0d0bf086b
razdvapoka <razdvapoka@gmail.com>
2020-05-14 22:54:22 +0300

    :sparkles: Add links to curator pages

--- src/components/curators-section/index.js
+++ src/components/curators-section/index.js
@@ -9,38 +9,42 @@ import Markdown from "../markdown"
 import Section from "../section"
 import styles from "./index.module.styl"
 
-const Curator = ({ curator, start, end, isInProgress }) => (
-  <IntlContextConsumer>
-    {({ language }) => (
-      <div className="w-1/4 bg-lightGrey">
-        <div
-          className={cn(
-            styles.time,
-            "flex items-center justify-between pl-2 pr-18 text-xs-F uppercase bg-pale"
-          )}
-        >
-          <div>{format(start, "dd MMMM", language === "ru" ? { locale: ru } : {})}</div>
-          <div>
-            {format(start, "HH:mm")}-{format(end, "HH:mm")}
-          </div>
-        </div>
-        <div className="pt-10 pl-2 pr-12 pb-3">
-          <div className={cn("text-m-F uppercase", styles.name)}>{curator.name}</div>
-          <Markdown className={cn("text-xs-F-h overflow-hidden", styles.description)}>
-            {curator.description.description}
-          </Markdown>
-          {curator.url && (
-            <div className={cn("text-xs-L uppercase mt-6", styles.more)}>
-              <a href={curator.url} {...blank()}>
-                <FormattedMessage id="curatorMore" />
-              </a>
+const Curator = ({ curator, start, end, isInProgress }) => {
+  const Component = curator.url ? "a" : "div"
+  const props = curator.url ? { href: curator.url, ...blank() } : {}
+  return (
+    <IntlContextConsumer>
+      {({ language }) => (
+        <Component className={cn("w-1/4 bg-lightGrey block", styles.curator)} {...props}>
+          <div
+            className={cn(
+              styles.time,
+              "flex items-center justify-between pl-2 pr-18 text-xs-F uppercase bg-pale"
+            )}
+          >
+            <div>{format(start, "dd MMMM", language === "ru" ? { locale: ru } : {})}</div>
+            <div>
+              {format(start, "HH:mm")}-{format(end, "HH:mm")}
             </div>
-          )}
-        </div>
-      </div>
-    )}
-  </IntlContextConsumer>
-)
+          </div>
+          <div className="pt-10 pl-2 pr-12 pb-3">
+            <div className={cn("text-m-F uppercase", styles.name)}>{curator.name}</div>
+            <Markdown className={cn("text-xs-F-h overflow-hidden", styles.description)}>
+              {curator.description.description}
+            </Markdown>
+            {curator.url && (
+              <div className={cn("text-xs-L uppercase mt-6", styles.more)}>
+                <a href={curator.url} {...blank()}>
+                  <FormattedMessage id="curatorMore" />
+                </a>
+              </div>
+            )}
+          </div>
+        </Component>
+      )}
+    </IntlContextConsumer>
+  )
+}
 
 const CuratorsSection = ({ curatorDays, ...rest }) => {
   return (
